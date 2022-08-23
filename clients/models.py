115c5046edea2cb1b1ca8dadd00dc6d15ac75ea7
Nuriia-1987 <tatarochka-9@mail.ru>
2022-08-23 18:59:20 +0600

    admin and models

--- clients/models.py
+++ clients/models.py
@@ -14,3 +14,24 @@ class Client(models.Model):
         null=True,
         blank=True
     )
+
+class Order(models.Model):
+    created_at = models.DateTimeField(
+        verbose_name="Дата и время создания заказа",
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="Дата и время изменения заказа",
+        auto_now=True,
+    )
+    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=255)
+    contacts = models.CharField(max_length=255)
+    finished = models.BooleanField(default=False)
+
+    def __init__(self):
+        return f"{self.name} - {self.contacts}"
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
